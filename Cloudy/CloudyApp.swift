450f6ced3640d73955d1f8a60f28cdff663e82b3
guillermoSb <guillermo.santosba@gmail.com>
2021-06-19 11:15:10 -0600

    Initial Commit

---
+++ Cloudy/CloudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudyApp.swift
+//  Cloudy
+//
+//  Created by Guille on 19/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CloudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
